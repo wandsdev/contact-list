c370f829a0399586509f3405445f4487cd5fc54c
wands <wandsdeveloper@gmail.com>
2022-04-21 12:29:30 -0300

    create componente contact-list

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card class="mx-auto">
+    <v-list-item>
+      <v-list-item-avatar size="56">
+        <v-img :src="contact.image"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-content class="ml-4">
+        <v-list-item-title class="title" v-html="contact.name"></v-list-item-title>
+
+        <div class="d-flex align-center mt-2">
+          <v-icon color="purple">mdi-phone</v-icon>
+          <span class="body-2 ml-2">{{ contact.contact }}</span>
+        </div>
+
+        <div class="d-flex align-center mt-2">
+          <v-icon color="purple">mdi-email</v-icon>
+          <span class="body-2 ml-2">{{ contact.email }}</span>
+        </div>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider class="purple mx-4"></v-divider>
+
+    <v-card-actions>
+      <v-list-item>
+        <v-spacer></v-spacer>
+        <v-btn class="mb-0" color="purple" dark fab small>
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn class="ml-5" color="purple" dark fab small>
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+        <v-btn class="ml-5" color="purple" dark fab small>
+          <v-icon>mdi-card-account-details</v-icon>
+        </v-btn>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ContactCard',
+  props: {
+    contact: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    <h1>Conatc List</h1>
+    <v-col offset-xs="1" xs="10" offset-sm="1" sm="10" offset-md="3" md="6" offset-lg="4" lg="4" >
+      <v-list v-for="item in contacts" :key="item.id">
+        <ContactCard :contact="item" />
+      </v-list>
+    </v-col>
   </div>
 </template>
 
 <script>
-export default {};
+import ContactCard from '../components/ContactCard.vue';
+
+export default {
+  components: { ContactCard },
+  data: () => ({
+    contacts: [
+      {
+        id: 1,
+        name: 'test',
+        contact: '965896745',
+        email: 'test@gmail.com',
+        image: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      },
+      {
+        id: 2,
+        name: 'test 2',
+        contact: '56894161',
+        email: 'test@gmail.com',
+        image: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      },
+    ],
+  }),
+};
 </script>
 
 <style lang="scss" scoped></style>
